0c9bb71a684b70c9fdfcc7fff6c09d5e3e7d855a
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-04-09 19:16:57 +0900

    [Bronze V] Title: A+B - 8, Time: 136 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = +input[0];
+let answer = '';
+
+for (let i = 1; i <= T; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    
+    answer += `Case #${i}: ${A} + ${B} = ${A + B}\n`;
+}
+
+console.log(answer);
