fa6ea67e91855179fcee58f45b3a36f2578062d9
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-04-10 12:37:49 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -1,26 +1,14 @@
-const readline = require('readline');
+const fs = require('fs');
+const [x, y] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n').map(Number);
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const input = [];
-
-rl.on('line', (line) => {
-    input.push(+line);
-}).on('close', () => {
-    const [x, y] = input;
-    
-    if (x > 0 && y > 0) {
-        console.log(1);
-    } else if (x < 0 && y > 0) {
-        console.log(2);
-    } else if (x < 0 && y < 0) {
-        console.log(3);
-    } else if (x > 0 && y < 0) {
-        console.log(4);
-    }
-    
-    process.exit();
-});
+if (x > 0 && y > 0) {
+    console.log(1);
+} else if (x < 0 && y > 0) {
+    console.log(2);
+} else if (x < 0 && y > 0) {
+    console.log(2);
+} else if (x < 0 && y < 0) {
+    console.log(3);
+} else if (x > 0 && y < 0) {
+    console.log(4);
+}
