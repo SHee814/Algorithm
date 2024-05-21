20c620960294bbc39fc6c863419e2c8b685de96d
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-05-21 18:24:29 +0900

    [Silver III] Title: N과 M (4), Time: 188 ms, Memory: 12000 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,28 @@
+const [N, M] = require('fs').readFileSync(0, 'utf-8').toString().trim().split(' ').map(Number);
+const dx = Array.from(Array(N), (_, i) => i + 1);
+let answer = '';
+
+function dfs(x, sequence) {
+    if (sequence.length === M) {
+        answer += `${sequence.join(' ')}\n`;
+        return;
+    }
+    
+    const maxNum = sequence[sequence.length - 1];
+    
+    for (let nx of dx) {
+        if (nx < maxNum) continue;
+        
+        sequence.push(nx);
+        dfs(nx, sequence);
+        sequence.pop();
+    }
+}
+
+for (let x of dx) {
+    const sequence = [x];
+    
+    dfs(x, sequence);
+}
+
+console.log(answer);
