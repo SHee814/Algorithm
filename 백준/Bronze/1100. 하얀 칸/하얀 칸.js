239b3af0215da0cff829a5890d0c3c51fed07c13
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-05-27 19:40:10 +0900

    [Bronze II] Title: 하얀 칸, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.js
@@ -0,0 +1,10 @@
+const board = require('fs').readFileSync(0, 'utf-8').toString().trim().split('\n').map(x => x.split(''));
+let count = 0;
+
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 === 0 && board[i][j] === 'F') count++;
+    }
+}
+
+console.log(count);
