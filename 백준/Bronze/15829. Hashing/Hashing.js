a31055275c26d4e00899d4eb577d3658b52cc184
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-05-29 15:32:57 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 9344 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,8 +1,10 @@
 const input = require('fs').readFileSync(0, 'utf-8').toString().trim().split('\n');
 const str = input[1];
-const key = str.split('').reduce((acc, x, i) => {
-    return acc + (x.charCodeAt() - 96) * (31 ** i);
-}, 0);
-const value = key % 1234567891;
+const mod = 1234567891n;
+const value = str.split('').reduce((acc, x, i) => {
+    const power = 31n ** BigInt(i);
+    const charCode = BigInt(x.charCodeAt() - 96);
+    return (acc + (charCode * power) % mod) % mod;
+}, 0n);
 
-console.log(value);
+console.log(value.toString());
