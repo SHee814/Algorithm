146edd664c400c2c3970286bf6d081f75aef0025
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-07-07 15:53:29 +0900

    [Bronze III] Title: 웰컴 키트, Time: 116 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.js
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.js
@@ -4,6 +4,6 @@ const N = +input[0];
 const shirt = input[1].split(' ').map(Number);
 const [T, P] = input[2].split(' ').map(Number);
 const packT = shirt.reduce((acc, x) => acc + Math.ceil(x / T), 0);
-const packP = Math.floor(N / P);
+const packP = ~~(N / P);
 const eachP = N % P;
 console.log(`${packT}\n${packP} ${eachP}`);
