1eb95508f417e8a990647d4db4950e1235b21d2c
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-07-17 12:10:36 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 2868 ms, Memory: 12324 KB -BaekjoonHub

--- 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.js
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.js
@@ -1,3 +1,3 @@
-const N = BigInt(require('fs').readFileSync(0, 'utf-8').toString());
+const N = BigInt(require('fs').readFileSync(0, 'utf-8'));
 const MOD = 20000303n;
 console.log((N % MOD).toString());
