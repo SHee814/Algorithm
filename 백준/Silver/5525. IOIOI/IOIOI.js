bfb4d50c1360421b602bd1e3acaff68ea92f1473
SHee814 <163508215+SHee814@users.noreply.github.com>
2024-11-12 13:02:06 +0900

    [Silver I] Title: IOIOI, Time: 168 ms, Memory: 11832 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -3,18 +3,21 @@ const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 const N = +input[0];
 const M = +input[1];
 const S = input[2];
-const P = Array.from(new Array(2 * N + 1), (_, i) => i % 2 ? 'O' : 'I').join('');
-let start = 0;
 let count = 0;
+let length = 0;
+let index = 0;
 
-while (start < M) {
-    const x = S.indexOf(P, start);
-    
-    if (x >= 0) {
-        count++;
-        start = x + 1;
+while (index < M - 2) {
+    if (S[index] === 'I' && S[index + 1] === 'O' && S[index + 2] === 'I') {
+        length++;
+        index += 2;
     } else {
-        break;
+        length = 0;
+        index++;
+    }
+    
+    if (length >= N) {
+        count++;
     }
 }
 
