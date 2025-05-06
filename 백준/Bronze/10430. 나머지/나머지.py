4af14c2dd7c2891f8d3b64995866eec832f96a8f
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-05-06 21:55:02 +0900

    [Bronze V] Title: 나머지, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,4 +1,6 @@
-import sys
+A, B, C = map(int, input().split())
 
-A, B, C = map(int, sys.stdin.readline().split())
-print(f"{(A+B)%C}\n{((A%C)+(B%C))%C}\n{((A*B)%C)}\n{(A%C)*(B%C)%C}")
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
