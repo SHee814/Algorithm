d5ee08aacd5cf4b88edbc53280309ad83e4abd43
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-05-06 21:49:33 +0900

    [Bronze V] Title: 사칙연산, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,4 +1,6 @@
-import sys
-
-a, b = map(int, sys.stdin.readline().split())
-print(f"{a+b}\n{a-b}\n{a*b}\n{a//b}\n{a%b}\n");
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
