280e15d71cc36460e4d54fc612b1d57eefc46ef2
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-05-07 22:42:16 +0900

    [Bronze II] Title: 다이얼, Time: 116 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.js
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -1,32 +1,17 @@
-const str = require('fs').readFileSync('/dev/stdin').toString().trim().split('');
-const dial = {
-    'A': 2,
-    'B': 2,
-    'C': 2,
-    'D': 3,
-    'E': 3,
-    'F': 3,
-    'G': 4,
-    'H': 4,
-    'I': 4,
-    'J': 5,
-    'K': 5,
-    'L': 5,
-    'M': 6,
-    'N': 6,
-    'O': 6,
-    'P': 7,
-    'Q': 7,
-    'R': 7,
-    'S': 7,
-    'T': 8,
-    'U': 8,
-    'V': 8,
-    'W': 9,
-    'X': 9,
-    'Y': 9,
-    'Z': 9
-};
-const time = str.reduce((acc, s) => acc + dial[s] + 1, 0);
+const fs = require('fs');
+const chars = fs.readFileSync('/dev/stdin').toString().trim().split('');
+const dials = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ'];
+const timeTaken = [];
 
-console.log(time);
+chars.forEach(char => {
+    dials.forEach((dial, i) => {
+        if (dial.search(char) >= 0) {
+            timeTaken.push(i + 2 + 1);
+            return;
+        }
+    });
+});
+
+const answer = timeTaken.reduce((acc, e) => acc + e);
+
+console.log(answer);
