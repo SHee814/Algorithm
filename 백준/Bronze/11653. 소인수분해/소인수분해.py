25f33dd08fa541d9f495e1d094ddfc3e71996f6f
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-05-10 22:01:03 +0900

    [Bronze I] Title: 소인수분해, Time: 188 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,21 +1,10 @@
 N = int(input())
-prime_numbers = [True for _ in range(N // 2 + 1)]
+i = 2
 
-for i in range(2, N // 2 + 1):
-    if not prime_numbers[i]:
+while N > 1:
+    if N % i == 0:
+        print(i)
+        N /= i
         continue
         
-    j = 2
-    
-    while i * j < N // 2 + 1:
-        prime_numbers[i * j] = False
-        j += 1
-
-for i in range(2, N // 2 + 1):
-    if prime_numbers[i]:
-        while N % i == 0:
-            print(i)
-            N /= i
-
-if N > 1:
-    print(N)
+    i += 1
