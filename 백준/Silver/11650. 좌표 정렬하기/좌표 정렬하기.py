fdd06cc3e5fc8812ffe634cfab85a6280d60443b
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-05-16 19:56:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 404 ms, Memory: 128568 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,11 @@
+N = int(input())
+coords = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    coords.append([x, y])
+
+coords.sort(key=lambda x: (x[0], x[1]))
+
+for i in range(N):
+    print(f'{coords[i][0]} {coords[i][1]}')
