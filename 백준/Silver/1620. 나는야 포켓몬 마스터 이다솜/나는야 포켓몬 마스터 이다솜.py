ee71d67a79b13331881a0d0791cf665befc88569
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-05-17 11:58:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 236 ms, Memory: 181328 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,15 +1,15 @@
-N, M = map(int, input().split())
+import sys
+input = sys.stdin.read().split('\n')
+
+N, M = map(int, input[0].split())
 dictionary = {}
 
-for i in range(N):
-    dictionary[input()] = i + 1
+for i in range(1, N + 1):
+    name = input[i]
+    dictionary[name] = i
+    dictionary[str(i)] = name
 
-dictionary_sorted = sorted(list(dictionary), key = lambda x: dictionary[x]) 
-   
-for _ in range(M):
-    q = input()
     
-    if q in dictionary:
-        print(dictionary[q])
-    else:
-        print(dictionary_sorted[int(q) - 1])
+for j in range(N + 1, N + M + 1):
+    q = input[j]
+    print(dictionary[q])
