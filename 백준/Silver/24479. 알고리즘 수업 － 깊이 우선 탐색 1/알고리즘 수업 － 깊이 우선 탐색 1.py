a25b477668ea8827d9ded69d1512c0fff6a39a29
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-08-17 15:48:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 336 ms, Memory: 165340 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,29 @@
+import sys
+
+N, M, R = map(int,input().split())
+E = [[] for _ in range(N)]
+visited = [0 for _ in range(N)]
+stack = [R]
+count = 1
+
+for line in sys.stdin.readlines():
+    s, e = map(int, line.split())
+    E[s-1].append(e)
+    E[e-1].append(s)
+
+for line in E:
+    line.sort(reverse=True)
+
+while len(stack) > 0:
+    cn = stack.pop()
+
+    if visited[cn-1] > 0:
+        continue
+        
+    visited[cn-1] = count
+    count += 1
+    
+    for nn in E[cn-1]:
+        stack.append(nn)
+
+print('\n'.join(map(str, visited)))
