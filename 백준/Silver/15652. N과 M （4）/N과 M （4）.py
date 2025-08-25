05ea5d4e6ed5f953ec70494a802e5dd80d54039e
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-08-25 15:55:40 +0900

    [Silver III] Title: N과 M (4), Time: 188 ms, Memory: 112892 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+def dfs(x, seq):
+    seq.append(x)
+    
+    if len(seq) == M:
+        print(*seq)
+        
+    for v in range(x, N+1):
+        if len(seq) < M:
+            dfs(v, seq)
+        
+    seq.pop()
+    
+for i in range(1, N+1):
+    dfs(i, [])
