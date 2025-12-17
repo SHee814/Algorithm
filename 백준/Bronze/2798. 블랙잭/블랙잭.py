cdd0b70b402e1c487b09ed5d5e55dfa1fe086222
SHee814 <163508215+SHee814@users.noreply.github.com>
2025-12-17 20:55:51 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,13 @@
 N, M = map(int, input().split())
 cards = list(map(int, input().split()))
-min_sum = 0
+mv = 0
 
-for i in range(0, N-2):
+for i in range(N-2):
     for j in range(i+1, N-1):
         for k in range(j+1, N):
-            sum = cards[i] + cards[j] + cards[k]
+            sum = cards[i]+cards[j]+cards[k]
             
-            if sum <= M:
-                min_sum = max(sum, min_sum)
-
-print(min_sum)
+            if (sum <= M):
+                mv = max(mv, sum)
+                
+print(mv)
