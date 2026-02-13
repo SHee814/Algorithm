751e9175184ed4aa9c2af96d7691af4c8e9b8a3f
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-02-13 19:47:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,10 +1,15 @@
-a, b, c = map(int, input().split())
+# 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+# 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+# 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
 
-if (a == b and b == c):
-    print(10000 + a * 1000)
-elif (a == b or b == c):
-    print(1000 + b * 100)
-elif (a == c):
-    print(1000 + a * 100)
+A, B, C = map(int, input().split())
+
+if A == B and B == C:
+    print(A * 1_000 + 10_000)
+elif A != B and B != C and C != A:
+    print(max(A, B, C) * 100)
 else:
-    print(100 * max(a, b, c))
+    if A == B or A == C:
+        print(A * 100 + 1_000)
+    else:
+        print(B * 100 + 1_000)
