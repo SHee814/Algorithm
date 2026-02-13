21d2a6777c0d18c430d4f5c5e9b0c21b05e673c3
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-02-13 19:21:16 +0900

    [Bronze V] Title: 윤년, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,5 @@
-year = int(input())
+# 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
 
-if (year % 4 == 0) and (year % 400 == 0 or year % 100 > 0):
-    print(1)
-else:
-    print(0)
+y = int(input())
+
+print(1 if y % 4 == 0 and (y % 400 == 0 or y % 100 > 0) else 0)
