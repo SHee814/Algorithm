e7a260259d53222b5c479499a48dbc3d6bf1caa2
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-02-15 12:13:50 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int X, N;
+    std::cin >> X;
+    std::cin >> N;
+    int sum = 0;
+    
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        sum += a * b;
+    }
+    
+    if (sum == X) {
+        std::cout << "Yes";
+    } else {
+        std::cout << "No";
+    }
+}
