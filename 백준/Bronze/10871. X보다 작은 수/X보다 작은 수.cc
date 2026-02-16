261efc9eb0c8a90811a46f464ae1305ea8040774
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-02-16 15:44:24 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,21 +1,13 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <sstream>
-#include <string>
-#include <iterator>
 
 int main() {
-    int N, X;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int N, X, v;
     std::cin >> N >> X;
-    std::cin.ignore();
-    std::string line;
-    std::getline(std::cin, line);
-    std::stringstream ss(line);
-    std::vector<int> nums((std::istream_iterator<int>(ss)), std::istream_iterator<int>());
-    nums.erase(std::remove_if(nums.begin(), nums.end(), [X](int n) { return n >= X; }), nums.end());
-    std::ostringstream oss;
-    std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(oss, " "));
-    std::string result = oss.str();
-    std::cout << result;
+    
+    for (int i = 0; i < N; i++) {
+        std::cin >> v;
+        if (v < X) std::cout << v << " ";
+    }
 } 
