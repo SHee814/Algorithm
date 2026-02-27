8ef8298e5692f1c0cbfb671bd6cf9bdc767992e9
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-02-27 16:00:40 +0900

    [Silver IV] Title: 스택 2, Time: 760 ms, Memory: 289156 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,20 +1,19 @@
 import sys
 
-input = sys.stdin.read().rstrip().split('\n')
-N = int(input[0])
+N = int(input())
+commands = [[int(t) for t in line.split()] for line in sys.stdin]
 stack = []
 
-for i in range(1, N + 1):
-    line = list(map(int, input[i].split()))
-    
-    if line[0] == 1:
-        stack.append(line[1])
-    elif line[0] == 2:
-        print(stack.pop() if len(stack) else -1)
-    elif line[0] == 3:
-        print(len(stack))
-    elif line[0] == 4:
-        print(1 if len(stack) == 0 else 0)
-    elif line[0] == 5:
-        print(stack[len(stack) - 1] if len(stack) > 0 else -1)
-    
+for cmd in commands:
+    match cmd[0]:
+        case 1:
+            stack.append(cmd[1])
+        case 2:
+            print(stack.pop() if stack else -1)
+        case 3:
+            print(len(stack))
+        case 4:
+            print(0 if stack else 1)
+        case 5:
+            print(stack[-1] if stack else -1)
+            
