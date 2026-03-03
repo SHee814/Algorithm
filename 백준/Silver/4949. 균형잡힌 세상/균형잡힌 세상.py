22518fb8676779feb354bdd6e4751b5306cf5068
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-03-03 21:36:16 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 120 ms, Memory: 112256 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,29 +1,32 @@
 import sys
 
-input = sys.stdin.read().rstrip().split('\n')
+for line in sys.stdin.read().splitlines():
+    if line == '.':
+        break
 
-for str in input:
-    if str == '.': break
-    
     stack = []
+    to_x = {'(':-1, ')':1, '[':-2, ']':2}
     flag = True
     
-    for i in str:
-        if i == '(':
-            stack.append(i)
-        elif i == ')':
-            if len(stack) > 0 and stack[len(stack) - 1] == '(':
-                stack.pop()
+    for s in line:
+        if s not in to_x:
+            continue
+            
+        x = to_x[s]
+        
+        if x < 0:
+            stack.append(x)
+        else:
+            if stack:
+                y = stack[-1]
+                
+                if x + y != 0:
+                    flag = False
+                    break
+                else:
+                    stack.pop()
             else:
                 flag = False
                 break
-        elif i == '[':
-            stack.append(i)
-        elif i == ']':
-            if len(stack) > 0 and stack[len(stack) - 1] == '[':
-                stack.pop()
-            else:
-                flag = False
-                break
-    
-    print('yes' if flag and len(stack) == 0 else 'no')
+                    
+    print('yes' if (not stack and flag) else 'no')
