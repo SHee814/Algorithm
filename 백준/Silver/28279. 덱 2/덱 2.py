b97527401c7298d1da647027bbc8c9fde0e300d5
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-03-04 01:25:07 +0900

    [Silver IV] Title: 덱 2, Time: 512 ms, Memory: 273912 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -2,25 +2,25 @@ import sys
 from collections import deque
 
 N = int(input())
-deq = deque()
+D = deque()
 
-for line in sys.stdin:
-    cmd = line.rstrip().split()
+for line in sys.stdin.read().splitlines():
+    cmd = line.split()
     
-    if cmd[0] == '1':
-        deq.appendleft(cmd[1])
-    elif cmd[0] == '2':
-        deq.append(cmd[1])
-    elif cmd[0] == '3':
-        print(deq.popleft() if len(deq) > 0 else -1)
-    elif cmd[0] == '4':
-        print(deq.pop() if len(deq) > 0 else -1)
-    elif cmd[0] == '5':
-        print(len(deq))
-    elif cmd[0] == '6':
-        print(1 if len(deq) == 0 else 0)
-    elif cmd[0] == '7':
-        print(deq[0] if len(deq) > 0 else -1)
-    elif cmd[0] == '8':
-        print(deq[-1] if len(deq) > 0 else -1)
-    
+    match cmd[0]:
+        case '1':
+            D.appendleft(cmd[1])
+        case '2':
+            D.append(cmd[1])
+        case '3':
+            print(D.popleft() if D else -1)
+        case '4':
+            print(D.pop() if D else -1)
+        case '5':
+            print(len(D))
+        case '6':
+            print(1 if not D else 0)
+        case '7':
+            print(D[0] if D else -1)
+        case '8':
+            print(D[-1] if D else -1)
