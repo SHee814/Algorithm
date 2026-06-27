de43ceaa7d3f031598f84eb313c6be376532eea2
SHee814 <163508215+SHee814@users.noreply.github.com>
2026-06-27 20:57:27 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.27 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,5 +1,10 @@
 def solution(A,B):
+    answer = 0
     sA = sorted(A)
     sB = sorted(B, reverse=True)
-    result = sum([a * b for a, b in zip(sA, sB)])
-    return result
+    for i in range(len(A)):
+        a = sA[i]
+        b = sB[i]
+        answer += a * b
+    
+    return answer
